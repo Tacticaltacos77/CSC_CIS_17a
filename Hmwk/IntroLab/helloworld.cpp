3bb6a98b53525409b35af8bb0204fd7f463529d6
Tacticaltacos77 <JTFREG777@GMAIL.COM>
2025-09-06 14:50:29 -0700

    Add files via upload

---
+++ Hmwk/IntroLab/helloworld.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.cpp
+ * Author: James Tyler Fregeau
+ * Created on Sept 6,2025 2:15 pm
+ * Purpose:  Intro Lab
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+
+    cout << "Hello World"<< endl;
+
+}
